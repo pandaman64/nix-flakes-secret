99fa9d02ba060723294eb98d36d3c4297b9bcbd5
pandaman64 <kointosudesuyo@infoseek.jp>
2021-12-25 16:33:16 +0900

    minimal repro

--- flake.nix
+++ flake.nix
@@ -1,11 +1,5 @@
 {
-  description = "A very basic flake";
-
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+    packages.x86_64-linux.secret = nixpkgs.legacyPackages.x86_64-linux.writeText "secret" (builtins.readFile ./secret);
   };
 }
